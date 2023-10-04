3b2cd95ddb7360a13f1dbff3b7524027a673231f
adonak <andy.donakowski@gmail.com>
2023-10-03 22:41:08 -0500

    updated with completed Activity 6

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ function calculateMinValue(data){
         for(var year = 2015; year <= 2021; year+=1){
               //get temp for current year
               var value = NAME.properties["TAVG_"+ String(year)];
-              console.log(value)
               allValues.push(value)
         }
     }
@@ -54,9 +53,9 @@ function calcPropRadius(attValue) {
 };
 
 //function to convert markers to circle markers
-function pointToLayer(feature, latlng){
+function pointToLayer(feature, latlng, attributes){
     //Determine which attribute to visualize with proportional symbols
-    var attribute = "TAVG_2015";
+    var attribute = attributes[0];
 
     //create marker options
     var options = {
@@ -77,7 +76,7 @@ function pointToLayer(feature, latlng){
     var layer = L.circleMarker(latlng, options);
 
     //build popup content string
-    var popupContent = "<p><b>Weather Station:</b> " + feature.properties.NAME + "</p><p><b>" + "Ave. Temp. " + attribute.slice(-4) + ":</b> " + feature.properties[attribute] + "</p>";
+    var popupContent = "<p><b>Weather Station:</b> " + feature.properties.NAME + "</p><p><b>" + "Ave. Temp. " + attribute.slice(-4) + ":</b> " + feature.properties[attribute] + " F" + "</p>";
 
     //bind the popup to the circle marker
     layer.bindPopup(popupContent);
@@ -88,11 +87,109 @@ function pointToLayer(feature, latlng){
 
 
 //Add circle markers for point features to the map
-function createPropSymbols(data){
+function createPropSymbols(data, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
-        pointToLayer: pointToLayer
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
     }).addTo(mapWiscTemps);
+};
+
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    // add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+
+    //add icons to directional buttons
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.svg'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.svg'>") 
+
+    // click listener for buttons
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+
+            //increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                // if past the last attribute, wrap around to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                // if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
+            };
+
+            //update slider
+            document.querySelector('.range-slider').value = index;
+
+            // pass new attribute to update symbols
+            updatePropSymbols(attributes[index]);
+        })
+
+    })
+
+    // input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){            
+        var index = this.value;
+        // pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+// build attributes array from data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with avg temp values
+        if (attribute.indexOf("TAVG") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};
+
+//Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    mapWiscTemps.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>City:</b> " + props.NAME + "</p>";
+
+            //add formatted attribute to panel content string
+            var year = attribute.split("_")[1];
+            popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + " million</p>";
+
+            //update popup content            
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
+        };
+    });
 };
 
 
@@ -104,10 +201,14 @@ function getData(){
             return response.json();
         })
         .then(function(json){
+            // create attribute array
+            var attributes = processData(json);
             //calculate minimum data value
             minValue = calculateMinValue(json);
             //call function to create proportional symbols
-            createPropSymbols(json);
+            createPropSymbols(json, attributes);
+            // call function to create slider control
+            createSequenceControls(attributes);
         })
 };
 
